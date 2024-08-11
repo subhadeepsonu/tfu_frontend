d42b1b50f0c46f8ef13e0df20a34966c8a41a05a
subhadeepsonu <subhadeepthandaka@gmail.com>
2024-08-11 23:52:29 +0530

    k

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom"
+import { Route, Routes} from "react-router-dom"
 import HomePage from "./pages/home"
 import LoginPage from "./pages/login"
 import Adminpage from "./pages/Admin"
